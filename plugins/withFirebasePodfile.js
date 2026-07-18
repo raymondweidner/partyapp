8312f93e69fc7301447501d47e76df5e3adf31dc
Ray Weidner <raymond.weidner@gmail.com>
2026-07-17 20:32:09 -0400

    Fix for pods

---
+++ plugins/withFirebasePodfile.js
@@ -0,0 +1,36 @@
+const { withDangerousMod } = require('@expo/config-plugins');
+const fs = require('fs');
+const path = require('path');
+
+const withFirebasePodfile = (config) => {
+  return withDangerousMod(config, [
+    'ios',
+    async (config) => {
+      const file = path.join(config.modRequest.platformProjectRoot, 'Podfile');
+      let contents = await fs.promises.readFile(file, 'utf8');
+
+      const postInstallBlock = `
+  installer.pods_project.targets.each do |target|
+    if ['RNFBApp', 'RNFBAuth', 'RNFBCrashlytics', 'RNFBFirestore', 'RNFBFunctions', 'RNFBMessaging', 'RNFBAnalytics'].include?(target.name)
+      target.build_configurations.each do |config|
+        config.build_settings['CLANG_ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES'] = 'YES'
+        config.build_settings['DEFINES_MODULE'] = 'NO'
+      end
+    end
+  end
+`;
+
+      if (contents.includes('post_install do |installer|')) {
+        contents = contents.replace(
+          /post_install do \\|installer\\|/,
+          `post_install do |installer|\n${postInstallBlock}`
+        );
+      }
+
+      await fs.promises.writeFile(file, contents, 'utf-8');
+      return config;
+    },
+  ]);
+};
+
+module.exports = withFirebasePodfile;
